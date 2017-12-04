9c2e9382ca08682a766b51302d1b0dcc7c3afc58
Richard Ygman <chygman@hotmail.com>
2017-12-04 15:06:55 +0100

    Goran
    I updated the program previous name was Goran-Krus-utbildning i changed so developers/learner can easier find the code and what they are linked to ex

---
+++ Goran/Program.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace Goran
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+
+            STARTMENU();
+
+        }
+
+        
+   
+
+       
+        static void INSTÄLLNINGARMENU()
+        {
+
+        }
+
+        static void INSTÄLLNINGAR()
+        {
+
+        }
+
+
+        static void CREDIT()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader("CREDIT_MONGO.txt", Encoding.GetEncoding("windows-1252")))
+                {
+                    string line;
+
+                    while((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                     
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+                Console.ReadKey(true);
+            }
+        }
+
+        static void WEBBROWSERMENU()
+        {
+            Console.WriteLine("1)chrome");
+            Console.WriteLine("2)firefox");
+            Console.WriteLine("3)opera");
+            Console.WriteLine("4)tillbaka");
+            ConsoleKeyInfo webbs;
+            webbs = Console.ReadKey(true);
+            WEBBROWSERS(webbs);
+
+        }
+
+        static void WEBBROWSERS(ConsoleKeyInfo input)
+        {
+            switch (input.KeyChar)
+            {
+                case '1':
+                    var cprs = new ProcessStartInfo("chrome.exe");
+                    cprs.Arguments = "https://github.com/Richardtheprogrammer";
+                    Process.Start(cprs);
+                    Console.Clear();
+                    STARTMENU();
+                    break;
+
+                case '2':
+                    Console.Title = "Firefox";
+                    var fprs = new ProcessStartInfo("firefox.exe");
+                    fprs.Arguments = "https://github.com/Richardtheprogrammer";
+                    Process.Start(fprs);
+                    Console.Clear();
+                    STARTMENU();
+                    break;
+
+                case '3':
+                    Console.Title = "Opera";
+                    var oprs = new ProcessStartInfo("opera.exe");
+                    oprs.Arguments = "https://github.com/Richardtheprogrammer";
+                    Process.Start(oprs);
+                    Console.Clear();
+                    STARTMENU();
+                    break;
+
+                case '4':
+                    Console.Clear();
+                    STARTMENU();
+                    break;
+
+            }
+        }
+
+        static void UPPDELNING()
+        {
+            Console.WriteLine("1)FÄRG");
+            Console.WriteLine("2)INSTÄLLNINGAR");
+            Console.WriteLine("3)CREDITS");
+            Console.WriteLine("4)GITHUB");
+            ConsoleKeyInfo Muppspelning;
+            Muppspelning = Console.ReadKey(true);
+            MENUUPPDELNINGEN(Muppspelning);
+
+         
+
+        }
+
+
+
+
+        //ändrad nedanför//
+        static void MENUUPPDELNINGEN(ConsoleKeyInfo input)
+        {
+            switch (input.KeyChar)
+            {
+                case '1':
+                    Console.Clear();
+                    FÄRGMENU();
+
+                    break;
+
+                case '2':
+                    Console.Clear();
+                    INSTÄLLNINGARMENU();
+                    break;
+
+                case '3':
+                    Console.Clear();
+                    CREDIT();
+                    Console.ReadKey(true);
+                    Console.Clear();
+                    STARTMENU();
+                    break;
+
+                case '4':
+                    Console.Clear();
+                    WEBBROWSERMENU();
+                    break;
+            }
+        }
+        
+        static void STARTMENU()
+        {
+            Console.Title = "MENU";
+            Console.WriteLine("       0   0");
+            Console.WriteLine("                                        ");
+            Console.WriteLine("00000  00000  00000   00000  0         0");
+            Console.WriteLine("0   0  0   0  0   0   0   0  0 0       0");
+            Console.WriteLine("0   0  0   0  00000   0   0  0   0     0");
+            Console.WriteLine("00000  0   0  000     00000  0     0   0");
+            Console.WriteLine("0      0   0  0  0    0   0  0       0 0");
+            Console.WriteLine("0      0   0  0   0   0   0  0        00");
+            Console.WriteLine("00000  00000  0    0  0   0  0         0");
+            UPPDELNING();
+        }
+
+        static void FÄRGER(ConsoleKeyInfo input)
+        {
+          
+            switch(input.KeyChar)
+            {
+                case '1':
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Clear();
+                    STARTMENU();
+                    break;
+
+                case '2':
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Clear();
+                    STARTMENU();
+                    break;
+
+                case '3':
+                    Console.ResetColor();
+                    Console.Clear();
+                    STARTMENU();
+                    break;
+
+                case '4':
+                    Console.Clear();
+                    STARTMENU();
+                    break;
+
+            }
+        }
+
+        static void FÄRGMENU()
+        {
+            Console.WriteLine("1)mörk");
+            Console.WriteLine("2)ljus");
+            Console.WriteLine("3)återställ");
+            Console.WriteLine("4)tillbaka");
+            ConsoleKeyInfo Gfärger;
+            Gfärger = Console.ReadKey(true);
+            FÄRGER(Gfärger);
+        }
+    }
+}
---
+++ Goran/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Goran")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Goran")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("13f3cc72-e2ec-4015-8984-30567c5a0230")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("2.2.4.6")]
+[assembly: NeutralResourcesLanguage("sv-SE")]
+
